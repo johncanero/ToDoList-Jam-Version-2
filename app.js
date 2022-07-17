df2c4a16bd7114e4b8a85aa02ac97499d842559c
John Canero <johncanero.official@gmail.com>
2022-07-18 00:44:42 +0800

    Mongoose Connect, Schema, and Items

--- app.js
+++ app.js
@@ -4,69 +4,86 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date = require(__dirname+"/date.js");
-
+const date = require(__dirname + "/date.js");
 
 const app = express();
 // DELETE ARRAY ITEMS
 
-
 // SET - UP view: EJS
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-// public > css (styles)
-app.use(express.static("public")); 
+app.use(bodyParser.urlencoded({ extended: true }));
+// PUBLIC > CSS AND IMAGES
+app.use(express.static("public"));
 
 // MONGOOSE CONNECT
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// ITEMS SCHEMA - MONGOOSE
+const itemsSchema = {
+  name: String,
+};
+
+// MONGOOSE MODEL
+const Item = mongoose.model("Item", itemsSchema);
 
 
-// GET function for home route
-app.get ("/", function(req, res){
-    const day = date.getDate(); //date module from date.js
-    res.render("list", {listTitle:day, newListItems: items});
+// MONGO ITEMS
+const item1 = new Item({
+  name: "Welcome to your ToDoList-Jam!",
+});
+
+const item2 = new Item({
+  name: "Tap + button to add an item.",
+});
+
+const item3 = new Item({
+  name: "click checkbox to delete item.",
 });
 
 
-// POST function for home route
-app.post("/", function(req, res){
-    // console.log(req.body);
-    const item = req.body.newItem;
+// GET function for home route = mongoDB
+app.get("/", function (req, res) {
+  res.render("list", { listTitle: "Today", newListItems: items });
+});
 
-    // pushing work items list (work route)
-    if(req.body.list === "Work list") {
-        workItems.push(item);
-        res.redirect("/work");
-        // pushing home route
-    }else{
-        items.push(item);
-        res.redirect("/");
-    }
+// POST function for home route
+app.post("/", function (req, res) {
+  // console.log(req.body);
+  const item = req.body.newItem;
+
+  // pushing work items list (work route)
+  if (req.body.list === "Work list") {
+    workItems.push(item);
+    res.redirect("/work");
+    // pushing home route
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
 // GET function for work route
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work list", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work list", newListItems: workItems });
 });
 
 // POST function for work route
-app.post("/work", function(req,res) {
-    const item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
+app.post("/work", function (req, res) {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 // GET function for about route
-app.get("/about", function(req, res) {
-    res.render("about");
-})
-
-// essentials: running server
-app.listen(process.env.PORT || 3000, function(){
-console.log("Server is running on port 3000");
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
-
-
-
-  
+// essentials: running server
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000");
+});
